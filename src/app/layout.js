073c209d8431a29f86a6a9f1637e0958ab8e389f
Victor Caruso <vic.caruso@gmail.com>
2023-09-07 18:53:13 -0700

    Add server side rendered footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {new Date().toLocaleString()}</footer>
       </body>
     </html>
   );
